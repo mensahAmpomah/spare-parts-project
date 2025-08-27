faf44677f77b80eb5bae6da87396825c0a3138a1
mensahAmpomah <gideonmensah201@gmail.com>
2025-08-27 07:54:49 -0700

    Set the Homepage to have images

--- carparts/models.py
+++ carparts/models.py
@@ -9,7 +9,7 @@ class Car(models.Model):
     year = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"{self.make} {self.model} ({self.year})"
+        return f"{self.make} "
 
 
 class SparePart(models.Model):
@@ -17,7 +17,7 @@ class SparePart(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to='media/sparparts_pics/', blank=True, null=True)
+    image = models.ImageField(upload_to='media/images/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} - {self.car}"
