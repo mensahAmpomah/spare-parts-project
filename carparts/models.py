6c63b0013beb15ec0c2e989044d85ad9959d2191
mensahAmpomah <gideonmensah201@gmail.com>
2025-08-28 07:26:27 -0700

    Added the car selections

--- carparts/models.py
+++ carparts/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
 class Car(models.Model):
     make = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
     year = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='media/cars',blank=True,null=True)
 
     def __str__(self):
         return f"{self.make} "
--- carparts/views.py
+++ carparts/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Car, SparePart
+from .models import Car
 
 def select_car(request):
     cars = Car.objects.all()
     return render(request, 'carparts/selectcar.html', {'cars': cars})
 
-# def view_spare_parts(request, car_id):
-#     car = get_object_or_404(Car, id=car_id)
-#     spare_parts = car.SparePart.all()  # Uses related_name
-#     return render(request, 'view_spare_parts.html', {'car': car, 'spare_parts': spare_parts})
+def view_spare_parts(request, car_id):
+    car = get_object_or_404(Car, id=car_id)
+    spare_parts = car.spare_parts.all()  
+    return render(request, 'carparts/view_spare_parts.html', {'car': car, 'spare_parts': spare_parts})
 
