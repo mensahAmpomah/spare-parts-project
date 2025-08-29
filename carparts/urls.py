8cf7d83cfe95a8e3736baa466fbcfcf9abf6f01a
mensahAmpomah <gideonmensah201@gmail.com>
2025-08-29 04:47:10 -0700

    added the shops pages and shop details

--- carparts/urls.py
+++ carparts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('select-car/', views.select_car, name='select-car'),
     path('spare-parts/<int:car_id>/', views.view_spare_parts, name='spare_parts'),
+    path("shops/", views.shop_list, name="shop_list"),
+    path("shops/<int:shop_id>/", views.shop_detail, name="shop_detail"),
 ]
--- carparts/views.py
+++ carparts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Car
+from .models import Car, Shop
 
 def select_car(request):
     cars = Car.objects.all()
@@ -9,4 +9,14 @@ def view_spare_parts(request, car_id):
     car = get_object_or_404(Car, id=car_id)
     spare_parts = car.spare_parts.all()  
     return render(request, 'carparts/view_spare_parts.html', {'car': car, 'spare_parts': spare_parts})
+
+
+def shop_list(request):
+    shops = Shop.objects.all()
+    return render(request, "carparts/shop_list.html", {"shops": shops})
+
+def shop_detail(request, shop_id):
+    shop = get_object_or_404(Shop, id=shop_id)
+    spare_parts = shop.shop_spare_parts.all()
+    return render(request, "carparts/shop_detail.html", {"shop": shop, "spare_parts": spare_parts})
 
